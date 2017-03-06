d3ede03b7c2f6175ce1415f08d63a2c044777711
Vyacheslav <mp314.96@gmail.com>
2017-03-06 18:13:53 +0500

    Добавлен popup contact-us

---
+++ www/blocks/popup-contactus.php
@@ -0,0 +1,10 @@
+<div id="popup-contactus">
+    <h2>обратная связь</h2>
+    <form action="send.php">
+        <input type="text" name="name" placeholder="Ваше имя или название организации">
+        <input type="email" name="email" placeholder="E-mail">
+        <input type="tel" name="tel" placeholder="Контектный телефон">
+        <textarea name="message" cols="30" rows="10"></textarea>
+        <input type="submit">
+    </form>
+</div>
