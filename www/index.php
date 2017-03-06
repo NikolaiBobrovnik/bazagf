413421b5817877de351046ff0899121ff7286758
Gleb Gorokhov <gg@verstaburo.com>
2017-03-06 13:45:19 +0300

    First commit

---
+++ www/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <?php include "blocks/head.php"; ?>
+    <meta name="robots" content="all">
+    <?php include "blocks/meta.php"; ?>
+    <title></title>
+    <?php include "blocks/css.php"; ?>
+</head>
+
+<body> 
+
+<header>
+    <div class="container">
+    </div>
+</header>
+
+<section class="">
+    <div class="container">
+    </div>
+</section>
+
+<footer>
+    <div class="container">
+    </div>
+</footer>
+
+<?php include "blocks/forms.php"; ?>
+<?php include "blocks/scripts.php"; ?>
+</body>
+</html>
