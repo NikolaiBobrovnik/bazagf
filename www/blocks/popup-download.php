29649a019536bbf6000ba4aa7ad7f4a86f6a184b
Vyacheslav <mp314.96@gmail.com>
2017-03-06 18:22:27 +0500

    HTML наполнение для popup

---
+++ www/blocks/popup-download.php
@@ -0,0 +1,11 @@
+<div id="popup-download">
+    <h2>Полная информация о компании</h2>
+    <span>Если скачивание не началось, то перейдите по <a href="#">ссылке</a></span>
+    <div id="popup-social">
+        <a href="#"><img src="../img/facebook-footer.png" alt=""></a>
+        <a href="#"><img src="../img/vk-footer.png" alt=""></a>
+        <a href="#"><img src="../img/ok-footer.png" alt=""></a>
+        <a href="#"><img src="../img/linkedin-footer.png" alt=""></a>
+        <a href="#"><img src="../img/youtube-footer.png" alt=""></a>
+    </div>
+</div>
---
+++ www/blocks/popup-thank-you.php
@@ -0,0 +1,11 @@
+<div id="popup-thank-you">
+    <h2>Спасибо за обращение</h2>
+    <span>Мы ответим на ваше обращение как можно скорее.</span>
+    <div id="popup-social">
+        <a href="#"><img src="img/facebook-footer.png" alt=""></a>
+        <a href="#"><img src="img/vk-footer.png" alt=""></a>
+        <a href="#"><img src="img/ok-footer.png" alt=""></a>
+        <a href="#"><img src="img/linkedin-footer.png" alt=""></a>
+        <a href="#"><img src="img/youtube-footer.png" alt=""></a>
+    </div>
+</div>
