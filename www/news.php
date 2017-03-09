c54938d218714fec8bbfc35cc292a26e9ccfdc22
mkochev741 <kochevm@ya.ru>
2017-03-09 09:33:49 +0300

    edit news

--- www/news.php
+++ www/news.php
@@ -12,47 +12,7 @@
 <body>
 
 <header>
-    <div class="container">
-        <div class="header-top">
-            <div class="date">
-                <span class="date-day">Среда , 8 Февраля 2017</span>
-            </div>
-            <div class="tel">
-                <a href="tel:+74957232832">+7 (495) 723 28 32</a>
-                <a href="tel:+79255327714">+7 (925) 532 77 14</a>
-            </div>
-            <div class="email">
-                <a href="mailto:info@bazagf.ru"></a>
-            </div>
-            <div class="social">
-                <a href="#" class="social-facebook" title="Facebook"></a>
-                <a href="#" class="social-vk" title="VK"></a>
-                <a href="#" class="social-ok" title="ok"></a>
-                <a href="#" class="social-linkedIn" title="LinkedIn"></a>
-                <a href="#" class="social-youtube" title="Youtube"></a>
-            </div>
-        </div>
-        <div class="header-middle">
-            <div class="logo">
-                <a href="/"><img src="img/logo.xxx" alt="Логотип" title="База ГФ"></a>
-            </div>
-            <div class="banner-one">
-                <a href="#"><img src="img/banner1" alt="Баннер1" title=""></a>
-            </div>
-            <div class="banner-two">
-                <a href="#"><img src="img/banner1" alt="Баннер2" title=""></a>
-            </div>
-        </div>
-        <div class="header-nav">
-            <ul class="header-list">
-                <li><a href="#" class="header-item">о нас</a></li>
-                <li><a href="#" class="header-item">купить</a></li>
-                <li><a href="#" class="header-item">продать</a></li>
-                <li><a href="#" class="header-item">новости</a></li>
-                <li><a href="#" class="header-item">контакты</a></li>
-            </ul>
-        </div>
-    </div>
+    <?php include "blocks/header.php"; ?>
 </header>
 
 <section class="news">
