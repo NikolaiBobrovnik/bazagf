77518b80a2f2f52c4b6f2b88c9eae6a2b25d7b68
Vyacheslav <mp314.96@gmail.com>
2017-03-10 19:11:13 +0500

    CSS стили футера

--- www/blocks/footer.php
+++ www/blocks/footer.php
@@ -1,18 +1,20 @@
 <div class="footer__top">
-  <div class="container">
+  <div class="container footer__container">
     <div class="footer__topcol">
-      <div class="footer__map" id="footer__map"></div>
+      <div class="footer__map" id="footer__map">
+          <img src="img/footer-map.png" alt="@@">
+      </div>
       <ul class="footer__contacts">
-        <li><img src="img/place-footer.png" alt="@@">129110, Москва, ул. Гиляровского, дом 57, стр. 1</li>
-        <li><img src="img/mail-footer.png" alt="@@">info@bazagf.ru</li>
-        <li><img src="img/phone-footer.png" alt="@@">+7 (495) 723 28 32<br>+7 (925) 532 77 14</li>
+        <li><p><img src="img/place-footer.png" alt="@@">129110, Москва, ул. Гиляровского, дом 57, стр. 1</p></li>
+        <li><p><img src="img/mail-footer.png" alt="@@"><a href="mailto:info@bazagf.ru">info@bazagf.ru</a></p></li>
+        <li><p><img src="img/phone-footer.png" alt="@@">+7 (495) 723 28 32<br>+7 (925) 532 77 14</p></li>
       </ul>
       <ul class="social-media footer__social">
-        <li><a href="#"><img src="img/facebook-footer.png" alt="@@"></a></li>
-        <li><a href="#"><img src="img/vk-footer.png" alt="@@"></a></li>
-        <li><a href="#"><img src="img/ok-footer.png" alt="@@"></a></li>
-        <li><a href="#"><img src="img/linkedin-footer.png" alt="@@"></a></li>
-        <li><a href="#"><img src="img/youtube-footer.png" alt="@@"></a></li>
+        <li><a href="#"><img src="img/facebook.png" alt="@@"></a></li>
+        <li><a href="#"><img src="img/vk.png" alt="@@"></a></li>
+        <li><a href="#"><img src="img/ok.png" alt="@@"></a></li>
+        <li><a href="#"><img src="img/linkedin.png" alt="@@"></a></li>
+        <li><a href="#"><img src="img/youtube.png" alt="@@"></a></li>
       </ul>
     </div>
     <div class="footer__topcol">
@@ -21,13 +23,13 @@
           <input type="email" name="email" placeholder="E-mail"><br>
           <input type="tel" name="tel" placeholder="Контактный телефон"><br>
           <textarea name="message" placeholder="Сообщение"></textarea><br>
-          <button type="submit" class="button"></button>
+          <button type="submit" class="button">Отправить</button>
       </form>
     </div>
   </div>
 </div>
 <div class="footer__bottom">
-  <div class="container">
+  <div class="container footer__bottom-row">
     <div class="footer__bottomcol">
       <p class="footer__copyright">BazaGF &copy; 2017</p>
       <div class="footer__agency">
@@ -36,44 +38,10 @@
       </div>
     </div>
     <ul class="footer__menu">
-      <li><a href="#">Главная</a></li>
-      <li><a href="#">О нас</a></li>
-      <li><a href="#">Купить</a></li>
-      <li><a href="#">Продать</a></li>
+      <li><p><a href="#">Главная</a></p></li>
+      <li><p><a href="#">О нас</a></p></li>
+      <li><p><a href="#">Купить</a></p></li>
+      <li><p><a href="#">Продать</a></p></li>
     </ul>
   </div>
 </div>
-
-<!--
-<div class="container">
-    <div id="footer-left">
-        <div id="footer-map"></div>
-        <p><img src="img/place-footer.png" alt="@@">129110, Москва, ул. Гиляровского, дом 57, стр. 1</p>
-        <p><img src="img/mail-footer.png" alt="@@">info@bazagf.ru</p>
-        <p><img src="img/phone-footer.png" alt="@@">+7 (495) 723 28 32<br>+7 (925) 532 77 14</p>
-        <div id="footer-social">
-            <a href="#"><img src="img/facebook-footer.png" alt="@@"></a>
-            <a href="#"><img src="img/vk-footer.png" alt="@@"></a>
-            <a href="#"><img src="img/ok-footer.png" alt="@@"></a>
-            <a href="#"><img src="img/linkedin-footer.png" alt="@@"></a>
-            <a href="#"><img src="img/youtube-footer.png" alt="@@"></a>
-        </div>
-    </div>
-    <div id="footer-right">
-        <form action="send.php">
-            <input type="text" name="name" placeholder="Ваше имя или название организации"><br>
-            <input type="email" name="e-mail" placeholder="E-mail"><br>
-            <input type="tel" name="telephone" placeholder="Контактный телефон"><br>
-            <textarea name="message" placeholder="Сообщение" cols="40" rows="3"></textarea><br>
-            <input type="submit"><br>
-        </form>
-    </div>
-    <div id="footer-nav">
-        <span><img src="img/bureau-footer.png" alt="@@">Сделало это сайт<br>в 2017м году </span>
-        <a href="#">Главная</a>
-        <a href="#">О нас</a>
-        <a href="#">Купить</a>
-        <a href="#">Продать</a>
-    </div>
-</div>
--->
