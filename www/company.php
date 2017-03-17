1c03e6b592659e28fcb4ce139d25f9b1e65f8868
Vyacheslav <mp314.96@gmail.com>
2017-03-17 17:59:41 +0500

    company.php

--- www/company.php
+++ www/company.php
@@ -11,48 +11,8 @@
 
 <body>
 
-<header>
-    <div class="container">
-        <div class="header-top">
-            <div class="date">
-                <span class="date-day">Среда , 8 Февраля 2017</span>
-            </div>
-            <div class="tel">
-                <a href="tel:+74957232832">+7 (495) 723 28 32</a>
-                <a href="tel:+79255327714">+7 (925) 532 77 14</a>
-            </div>
-            <div class="email">
-                <a href="mailto:info@bazagf.ru"></a>
-            </div>
-            <div class="social">
-                <a href="#" class="social-facebook" title="Facebook"></a>
-                <a href="#" class="social-vk" title="VK"></a>
-                <a href="#" class="social-ok" title="ok"></a>
-                <a href="#" class="social-linkedIn" title="LinkedIn"></a>
-                <a href="#" class="social-youtube" title="Youtube"></a>
-            </div>
-        </div>
-        <div class="header-middle">
-            <div class="logo">
-                <a href="/"><img src="img/logo.xxx" alt="Логотип" title="База ГФ"></a>
-            </div>
-            <div class="banner-one">
-                <a href="#"><img src="img/banner1" alt="Баннер1" title=""></a>
-            </div>
-            <div class="banner-two">
-                <a href="#"><img src="img/banner1" alt="Баннер2" title=""></a>
-            </div>
-        </div>
-        <div class="header-nav">
-            <ul class="header-list">
-                <li><a href="#" class="header-item">о нас</a></li>
-                <li><a href="#" class="header-item">купить</a></li>
-                <li><a href="#" class="header-item">продать</a></li>
-                <li><a href="#" class="header-item">новости</a></li>
-                <li><a href="#" class="header-item">контакты</a></li>
-            </ul>
-        </div>
-    </div>
+<header class="header">
+    <?php include "blocks/header.php"; ?>
 </header>
 
 <section class="company">
@@ -67,17 +27,17 @@
                <h2>ООО Дэни</h2>
                Полная информация о компании
            </div>
-           <div><span>Организационно-правовая форма</span>ООО</div>
-           <div><span>Год создания</span>28.10.2009</div>
-           <div><span>ОГРН</span>1097746664707</div>
-           <div><span>ИНФС</span>г. Москва</div>
-           <div><span>С/Н</span>усн 6%</div>
-           <div><span>Основной вид деятельности</span>Розничная торговля в неспециализированных магазинах</div>
-           <div><span>Дополнительный вид деятельности</span></div>
-           <div><span>Расчетный счет</span>Абсолют банк</div>
-           <div><span>Обороты</span>2014 год - 100 000 р.<br>2015 год - 765 000 р.<br>2016 год - 1 750 300 р.</div>
-           <div><span>Лицензия</span></div>
-           <div><span>Стоимость компании</span>60 000 р.</div>
+           <div><span>Организационно-правовая форма</span><p>ООО</p></div>
+           <div><span>Год создания</span><p>28.10.2009</p></div>
+           <div><span>ОГРН</span><p>1097746664707</p></div>
+           <div><span>ИНФС</span><p>г. Москва</p></div>
+           <div><span>С/Н</span><p>усн 6%</p></div>
+           <div><span>Основной вид деятельности</span><p>Розничная торговля в неспециализированных магазинах</p></div>
+           <div><span>Дополнительный вид деятельности</span><p></p></div>
+           <div><span>Расчетный счет</span><p>Абсолют банк</p></div>
+           <div><span>Обороты</span><p>2014 год - 100 000 р.<br>2015 год - 765 000 р.<br>2016 год - 1 750 300 р.</p></div>
+           <div><span>Лицензия</span><p></p></div>
+           <div><span>Стоимость компании</span><p>60 000 р.</p></div>
            <div class="company-content__footer">
                <a href="#">Закзать звонок</a>
                <a href="#">Бронировать</a>
