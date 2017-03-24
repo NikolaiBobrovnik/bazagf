60f263c3c7ace75115a5fa80a45bc8703f096a87
Vyacheslav <mp314.96@gmail.com>
2017-03-24 09:20:04 +0500

    Правки

--- www/index.php
+++ www/index.php
@@ -31,25 +31,55 @@
 
         <?php include "blocks/news.php"; ?>
 
-        <!-- <section class="popup-contactus">
-            <div class="popup__contact-us">
-                <section class="page-title popup-title">
-                    <div class="page-title__left-img"></div>
-                    <p>Новости</p>
-                    <div class="page-title__right-img"></div>
-                </section>
-                <form action="send.php" class="popup-contact-us">
-                    <input type="text" name="name" placeholder="Ваше имя или название организации">
-                    <input type="email" name="email" placeholder="E-mail">
-                    <input type="tel" name="tel" placeholder="Контектный телефон">
-                    <textarea name="message" cols="30" rows="10"></textarea>
-                    <input type="submit">
-                </form>
+        <section class="popup__contact-us">
+            <section class="popup__contact-us__title">
+                <p>ОБРАТНАЯ СВЯЗЬ</p>
+            </section>
+            <form action="send.php" class="popup__contact-us__form">
+                <input type="text" name="name" placeholder="Ваше имя или название организации">
+                <input type="email" name="email" placeholder="E-mail">
+                <input type="tel" name="tel" placeholder="Контектный телефон">
+                <textarea name="message" cols="30" placeholder="Сообщение" rows="10"></textarea>
+                <button type="submit" class="button button-blue">Отправить</button>
+            </form>
+        </section>
+        <section class="popup__small">
+            <div class="popup__small__header__gradient">
+                <div class="popup__small__header">
+                    <h2>Спасибо за обращение</h2>
+                    <p>Мы ответим на ваше сообщение<br>как можно скорее.</p>
+                </div>
             </div>
-        </section> -->
+            <div class="popup__small__footer">
+                <ul class="social-media">
+                    <li><a href="#" class="social-media__facebook"></a></li>
+                    <li><a href="#" class="social-media__vk"></a></li>
+                    <li><a href="#" class="social-media__ok"></a></li>
+                    <li><a href="#" class="social-media__linkedin"></a></li>
+                    <li><a href="#" class="social-media__youtube"></a></li>
+                </ul>
+            </div>
+        </section>
+
+        <section class="popup__small">
+            <div class="popup__small__header__gradient__download">
+                <div class="popup__small__header">
+                    <h2>Полная информация о<br>компании</h2>
+                    <p>Если скачивание не началось, то перейдите по <a href="#">ссылке</a></p>
+                </div>
+            </div>
+            <div class="popup__small__footer">
+                <ul class="social-media">
+                    <li><a href="#" class="social-media__facebook"></a></li>
+                    <li><a href="#" class="social-media__vk"></a></li>
+                    <li><a href="#" class="social-media__ok"></a></li>
+                    <li><a href="#" class="social-media__linkedin"></a></li>
+                    <li><a href="#" class="social-media__youtube"></a></li>
+                </ul>
+            </div>
+        </section>
 
         <?php include "blocks/footer.php"; ?>
-
         <?php include "blocks/forms.php"; ?>
         <?php include "blocks/scripts.php"; ?>
     </body>
